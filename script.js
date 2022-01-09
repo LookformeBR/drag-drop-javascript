8c71ed75cab9d9e067e9a77409ee20718fa399c9
Lucas Hilario Santos <lucashilarie@gmail.com>
2022-01-09 13:39:06 -0300

    pt-br

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-//selecting all required elements
+// selecionando todos os elementos necessários
 const dropArea = document.querySelector(".drag-area"),
 dragText = dropArea.querySelector("header"),
 button = dropArea.querySelector("button"),
 input = dropArea.querySelector("input");
-let file; //this is a global variable and we'll use it inside multiple functions
+let file; // esta é uma variável global e vamos usá-la dentro de várias funções
 
 button.onclick = ()=>{
-  input.click(); //if user click on the button then the input also clicked
+  input.click(); // se o usuário clicar no botão, a entrada também clicou
 }
 
 input.addEventListener("change", function(){
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  // obtendo o arquivo de seleção do usuário e [0] isso significa que se o usuário selecionar vários arquivos, selecionaremos apenas o primeiro
   file = this.files[0];
   dropArea.classList.add("active");
   showFile(); //calling function
 });
 
 
-//If user Drag File Over DropArea
+// Se o usuário arrastar arquivo sobre DropArea
 dropArea.addEventListener("dragover", (event)=>{
-  event.preventDefault(); //preventing from default behaviour
+  event.preventDefault(); // impedindo o comportamento padrão
   dropArea.classList.add("active");
-  dragText.textContent = "Release to Upload File";
+  dragText.textContent = "ReleasePara Fazer Upload de Arquivo";
 });
 
-//If user leave dragged File from DropArea
+// Se o usuário sair do arquivo arrastado de DropArea
 dropArea.addEventListener("dragleave", ()=>{
   dropArea.classList.remove("active");
-  dragText.textContent = "Drag & Drop to Upload File";
+  dragText.textContent = "Drag & Drop Para Fazer Upload de Arquivo";
 });
 
-//If user drop File on DropArea
+// Se o usuário soltar o arquivo no DropArea
 dropArea.addEventListener("drop", (event)=>{
-  event.preventDefault(); //preventing from default behaviour
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  event.preventDefault(); // impedindo o comportamento padrão
+  // obtendo o arquivo de seleção do usuário e [0] isso significa que se o usuário selecionar vários arquivos, selecionaremos apenas o primeiro
   file = event.dataTransfer.files[0];
-  showFile(); //calling function
+  showFile(); // chamando a função
 });
 
 function showFile(){
-  let fileType = file.type; //getting selected file type
-  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-  if(validExtensions.includes(fileType)){ //if user selected file is an image file
-    let fileReader = new FileReader(); //creating new FileReader object
+  let fileType = file.type; // obtendo o tipo de arquivo selecionado
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; // adicionando algumas extensões de imagem válidas na matriz
+  if(validExtensions.includes(fileType)){ // se o arquivo selecionado pelo usuário for um arquivo de imagem
+    let fileReader = new FileReader();// criando novo objeto FileReader
     fileReader.onload = ()=>{
-      let fileURL = fileReader.result; //passing user file source in fileURL variable
-      let imgTag = `<img src="${fileURL}" alt="">`; //creating an img tag and passing user selected file source inside src attribute
-      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+      let fileURL = fileReader.result; // passando a fonte do arquivo do usuário na variável fileURL
+      let imgTag = `<img src="${fileURL}" alt="">`; // criando uma tag img e passando a fonte do arquivo selecionado pelo usuário dentro do atributo src
+      dropArea.innerHTML = imgTag; // adicionando a tag img criada dentro do contêiner dropArea
     }
     fileReader.readAsDataURL(file);
   }else{
     alert("This is not an Image File!");
     dropArea.classList.remove("active");
-    dragText.textContent = "Drag & Drop to Upload File";
+    dragText.textContent = "Drag & Drop Para Fazer Upload de Arquivo";
   }
 }
